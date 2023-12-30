fdc4bdb5fccf2ecee35d1476a2ac93176460858f
Anna Varaksina <AVaraksina@mlg.ru>
2023-12-30 22:05:32 +0300

    structure

--- src/index.js
+++ src/index.js
@@ -1,68 +1,11 @@
-class SwapiService {
-  _baseURL = "https://swapi.dev/api";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app";
+import { createRoot } from "react-dom/client";
 
-  async getResources(url) {
-    const res = await fetch(`${this._baseURL}${url}`);
+// ReactDOM.render(<App />,
+//   document.getElementById('root'));
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
 
-    if (!res.ok) {
-      console.log("test2");
-      throw new Error("Could not fetch TEST"); // TODO не отображается в консоли
-    }
-
-    const body = await res.json();
-    return body;
-  }
-
-  async getAllPeople() {
-    const res = await this.getResources("/people/");
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getResources(`/people/${id}`);
-  }
-
-  async getAllPlanets() {
-    const res = await this.getResources("/planets/");
-    return res.results;
-  }
-  getPlanet(id) {
-    return this.getResources(`/planets/${id}`);
-  }
-  async getAllStarships() {
-    const res = await this.getResources("/starships/");
-    return res.results;
-  }
-  getStarship(id) {
-    return this.getResources(`/starships/${id}`);
-  }
-}
-
-const swapi = new SwapiService();
-
-swapi.getAllPeople().then((res) => {
-  res.forEach((el) => {
-    console.log(el.name);
-  });
-});
-swapi.getPerson(3).then((res) => {
-  console.log(`The person is ${res.name}`);
-});
-
-swapi.getAllPlanets().then((res) => {
-  res.forEach((el) => {
-    console.log(el.name);
-  });
-});
-swapi.getPlanet(3).then((res) => {
-  console.log(`The planet is ${res.name}`);
-});
-
-swapi.getAllStarships().then((res) => {
-  res.forEach((el) => {
-    console.log(el.name);
-  });
-});
-swapi.getStarship(3).then((res) => {
-  console.log(`The starship is ${res.name}`);
-});
+root.render(<App />);
