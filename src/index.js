9516c50442838bd08a2b4c561f0c15213aa20028
Anna Varaksina <AVaraksina@mlg.ru>
2023-12-30 14:51:45 +0300

    SwapiService as API client

--- src/index.js
+++ src/index.js
@@ -1,19 +1,68 @@
-const getResources = async (url) => {
-  const res = await fetch(url);
+class SwapiService {
+  _baseURL = "https://swapi.dev/api";
 
-  if (!res.ok) {
-    console.log("test2");
-    throw new Error(`Could not fetch test`);
+  async getResources(url) {
+    const res = await fetch(`${this._baseURL}${url}`);
+
+    if (!res.ok) {
+      console.log("test2");
+      throw new Error("Could not fetch TEST"); // TODO не отображается в консоли
+    }
+
+    const body = await res.json();
+    return body;
   }
 
-  const body = await res.json();
-  return body;
-};
+  async getAllPeople() {
+    const res = await this.getResources("/people/");
+    return res.results;
+  }
 
-getResources("https://swapi.dev/api/people/199999999")
-  .then((body) => {
-    console.log(body);
-  })
-  .catch(() => {
-    console.error(`Could not fetch`);
+  getPerson(id) {
+    return this.getResources(`/people/${id}`);
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResources("/planets/");
+    return res.results;
+  }
+  getPlanet(id) {
+    return this.getResources(`/planets/${id}`);
+  }
+  async getAllStarships() {
+    const res = await this.getResources("/starships/");
+    return res.results;
+  }
+  getStarship(id) {
+    return this.getResources(`/starships/${id}`);
+  }
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then((res) => {
+  res.forEach((el) => {
+    console.log(el.name);
   });
+});
+swapi.getPerson(3).then((res) => {
+  console.log(`The person is ${res.name}`);
+});
+
+swapi.getAllPlanets().then((res) => {
+  res.forEach((el) => {
+    console.log(el.name);
+  });
+});
+swapi.getPlanet(3).then((res) => {
+  console.log(`The planet is ${res.name}`);
+});
+
+swapi.getAllStarships().then((res) => {
+  res.forEach((el) => {
+    console.log(el.name);
+  });
+});
+swapi.getStarship(3).then((res) => {
+  console.log(`The starship is ${res.name}`);
+});
