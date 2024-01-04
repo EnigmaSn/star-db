8ae089d6f0a37b7e0e1934637f1b4f5d0a758367
Anna Varaksina <enigmasnape@gmail.com>
2024-01-04 19:54:12 +0300

    elements as props

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -4,6 +4,7 @@ import ItemList from "../item-list/item-list";
 import PersonDetails from "../person-details/person-details";
 import ErrorIndicator from "../error-indicator/error-indicator";
 import SwapiService from "../../services/swapi-service";
+import Row from "../row/row";
 
 import "./people-page.css";
 
@@ -30,21 +31,36 @@ export default class PeoplePage extends Component {
       return <ErrorIndicator />;
     }
 
+    const itemList = (
+      <ItemList
+        onItemSelected={this.onPersonSelected}
+        getData={this.swapiService.getAllPeople}
+        renderItem={(item) => {
+          return `${item.name} (${item.gender})`;
+        }}
+      />
+    );
+
+    const personDetails = (
+      <PersonDetails personId={this.state.selectedPerson} />
+    );
+
     return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList
-            onItemSelected={this.onPersonSelected}
-            getData={this.swapiService.getAllPeople}
-            renderItem={(item) => {
-              return `${item.name} (${item.gender})`;
-            }}
-          />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={this.state.selectedPerson} />
-        </div>
-      </div>
+      <Row left={itemList} right={personDetails} />
+      // <div className="row mb2">
+      //   <div className="col-md-6">
+      //     <ItemList
+      //       onItemSelected={this.onPersonSelected}
+      //       getData={this.swapiService.getAllPeople}
+      //       renderItem={(item) => {
+      //         return `${item.name} (${item.gender})`;
+      //       }}
+      //     />
+      //   </div>
+      //   <div className="col-md-6">
+      //     <PersonDetails personId={this.state.selectedPerson} />
+      //   </div>
+      // </div>
     );
   }
 }
