a2826eba03acf20884b6ef4c33dbf03184865652
Anna Varaksina <enigmasnape@gmail.com>
2024-01-06 15:13:18 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./components/app";
 import { createRoot } from "react-dom/client";
 
-// ReactDOM.render(<App />,
-//   document.getElementById('root'));
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
 
